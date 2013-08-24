956c6bcb95004596d1d3df18d75c4da739834b74
Dan Nixon <dan@dan-nixon.com>
2013-08-24 20:54:39 +0100

    Initia commit

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 RokuRTerm
 =========
 
-A very lightweight terminal based Roku remote
+A very lightweight terminal based [Roku](http://www.roku.com) remote.
+
+Uses [this](http://code.activestate.com/recipes/134892/) recipe to add cross platform ```getch()``` function.
+
+Config
+-----
+
+Simply set the ```roku_ip``` variable to the IP address of your Roku player, you can get this by "Setings" > "About" on your Roku player.
+
+You may wish to change some of the key mappings if you find my layout isn't working well for you, you can do this by changing the values in the ```case('X')``` statements.
+
+Usage
+-----
+
+Start the application using ```python RokuRTerm.py```, you may wish to enclose this is a shell script in your home directory allowing you to type something like ```./roku``` into a new terminal to launch the app.
+
+Once the app is running the following key functions are assigned:
+
+-	```?``` - Help
+-	```/``` - Exit Remote App
+-	```w``` - Up
+-	```a``` - Left
+-	```s``` - Down
+-	```d``` - Right
+-	```z``` - Select
+-	```q``` - Back
+-	```p``` - Play/Pause
+-	```<``` or ```,``` - Reverse
+-	```>``` or ```.``` - Forward
+-	```I``` - Info (* on Roku Remote)
+-	```R``` - Instant Replay
+
+This list can be obtained within the application by pressing ```?```.
---
+++ RokuRTerm.py
@@ -0,0 +1,143 @@
+import httplib, shlex
+
+##Set this to your Roku player IP
+roku_ip = "192.168.1.120"
+
+class _Getch:
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+getch = _Getch()
+
+class switch(object):
+	def __init__(self, value):
+		self.value = value
+		self.fall = False
+
+	def __iter__(self):
+		yield self.match
+		raise StopIteration
+
+	def match(self, *args):
+		if self.fall or not args:
+			return True
+		elif self.value in args:
+			self.fall = True
+			return True
+		else:
+			return False
+
+def send_key_cmd(cmd_string):
+	conn = httplib.HTTPConnection(roku_ip + ":8060")
+	conn.request("POST", "/keypress/" + cmd_string)
+
+def print_help():
+	print "RokuRTerm v0.1"
+	print "Commands:"
+        print "? - Help"
+        print "/ - Exit Remote App"
+	print "w - Up"
+	print "a - Left"
+        print "s - Down"
+        print "d - Right"
+        print "z - Select"
+        print "q - Back"
+        print "p - Play/Pause"
+        print "< or , - Reverse"
+        print "> or . - Forward"
+        print "I - Info (* on Roku Remote)"
+        print "R - Instant Replay"
+
+run = 1
+
+def parse_cl(cl_input):
+	global run
+	function = cl_input.upper()
+	for case in switch(function):
+		if case('/'):
+			run = 0
+			break
+		if case('P'):
+			send_key_cmd("Play")
+			break
+                if case('W'):
+                        send_key_cmd("Up")
+                        break
+                if case('A'):
+                        send_key_cmd("Left")
+                        break
+                if case('S'):
+                        send_key_cmd("Down")
+                        break
+                if case('D'):
+                        send_key_cmd("Right")
+                        break
+                if case('<'):
+                        send_key_cmd("Rev")
+                        break
+                if case(','):
+                        send_key_cmd("Rev")
+                        break
+                if case('>'):
+                        send_key_cmd("Fwd")
+                        break
+                if case('.'):
+                        send_key_cmd("Rev")
+                        break
+                if case('H'):
+                        send_key_cmd("Home")
+                        break
+		if case('Z'):
+			send_key_cmd("Select")
+			break
+                if case('Q'):
+                        send_key_cmd("Back")
+                        break
+                if case('R'):
+                        send_key_cmd("InstantReplay")
+                        break
+                if case('I'):
+                        send_key_cmd("Info")
+                        break
+		if case('?'):
+			print_help()
+			break
+def main():
+	print "Press / to exit, ? for help"
+	print "Roku IP: " + roku_ip
+	while(run):
+		c = getch()
+		parse_cl(c)
+
+main()
