c61b963a37ff1978243102c7218629ef73bde1bb
Dan Nixon <dan@dan-nixon.com>
2014-08-24 11:43:16 +0100

    Added warning on network error
    Fixed indentation

--- RokuRTerm.py
+++ RokuRTerm.py
@@ -6,7 +6,7 @@
 # 24/08/2013
 # Allows control of a Roku player over the external control API
 
-import httplib, sys
+import httplib, sys, socket
 
 ## Set this to your Roku player IP
 roku_ip = "192.168.1.120"
@@ -19,7 +19,6 @@ class Getch:
             self.impl = GetchUnix()
 
     def __call__(self): return self.impl()
-
 
 class GetchUnix:
     def __init__(self):
@@ -36,7 +35,6 @@ class GetchUnix:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-
 class GetchWindows:
     def __init__(self):
         import msvcrt
@@ -44,7 +42,6 @@ class GetchWindows:
     def __call__(self):
         import msvcrt
         return msvcrt.getch()
-
 
 getch = Getch()
 
@@ -67,28 +64,31 @@ class switch(object):
             return False
 
 def send_key_cmd(cmd_string):
-    conn = httplib.HTTPConnection(roku_ip + ":8060")
-    conn.request("POST", "/keypress/" + cmd_string)
-    conn.close()
+    try:
+        conn = httplib.HTTPConnection(roku_ip + ":8060")
+        conn.request("POST", "/keypress/" + cmd_string)
+        conn.close()
+    except socket.error:
+        print "Network error!"
 
 def print_help():
     print "RokuRTerm"
     print "Keys:"
-        print "? - Help"
-        print "/ - Exit Remote App"
+    print "? - Help"
+    print "/ - Exit Remote App"
     print "w - Up"
     print "a - Left"
-        print "s - Down"
-        print "d - Right"
-        print "z - Select"
-        print "b - Back"
-        print "q - Quit"
-        print "p - Play/Pause"
-        print "< or , - Reverse"
-        print "> or . - Forward"
-        print "i - Info (* on Roku Remote)"
-        print "r - Instant Replay"
-        print "k - keyboard mode"
+    print "s - Down"
+    print "d - Right"
+    print "z - Select"
+    print "b - Back"
+    print "q - Quit"
+    print "p - Play/Pause"
+    print "< or , - Reverse"
+    print "> or . - Forward"
+    print "i - Info (* on Roku Remote)"
+    print "r - Instant Replay"
+    print "k - keyboard mode"
 
 run = 1
 keymode = False
@@ -106,8 +106,8 @@ def parse_cl(cl_input):
         if cl_input == "\x08":
                 send_key_cmd("InstantReplay")
         if cl_input == "\x1b":
-                    keymode = False
-                    print "Exit keyboard mode"
+                keymode = False
+                print "Exit keyboard mode"
     else:
         for case in switch(function):
             if case('/'):
@@ -119,49 +119,49 @@ def parse_cl(cl_input):
             if case('P'):
                 send_key_cmd("Play")
                 break
-                if case('W'):
-                        send_key_cmd("Up")
-                        break
-                if case('A'):
-                        send_key_cmd("Left")
-                        break
-                if case('S'):
-                        send_key_cmd("Down")
-                        break
-                if case('D'):
-                        send_key_cmd("Right")
-                        break
-                if case('<'):
-                        send_key_cmd("Rev")
-                        break
-                if case(','):
-                        send_key_cmd("Rev")
-                        break
-                if case('>'):
-                        send_key_cmd("Fwd")
-                        break
-                if case('.'):
-                        send_key_cmd("Rev")
-                        break
-                if case('H'):
-                        send_key_cmd("Home")
-                        break
+            if case('W'):
+                send_key_cmd("Up")
+                break
+            if case('A'):
+                send_key_cmd("Left")
+                break
+            if case('S'):
+                send_key_cmd("Down")
+                break
+            if case('D'):
+                send_key_cmd("Right")
+                break
+            if case('<'):
+                send_key_cmd("Rev")
+                break
+            if case(','):
+                send_key_cmd("Rev")
+                break
+            if case('>'):
+                send_key_cmd("Fwd")
+                break
+            if case('.'):
+                send_key_cmd("Rev")
+                break
+            if case('H'):
+                send_key_cmd("Home")
+                break
             if case('Z'):
                 send_key_cmd("Select")
                 break
-                if case('B'):
-                        send_key_cmd("Back")
-                        break
-                if case('R'):
-                        send_key_cmd("InstantReplay")
-                        break
-                if case('I'):
-                        send_key_cmd("Info")
-                        break
-                if case('K'):
-                        keymode = True
-                        print "Entering keyboard mode, <ESC> to exit"
-                        break
+            if case('B'):
+                send_key_cmd("Back")
+                break
+            if case('R'):
+                send_key_cmd("InstantReplay")
+                break
+            if case('I'):
+                send_key_cmd("Info")
+                break
+            if case('K'):
+                keymode = True
+                print "Entering keyboard mode, <ESC> to exit"
+                break
             if case('?'):
                 print_help()
                 break
